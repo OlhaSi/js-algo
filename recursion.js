9d957b6c967b02229c06b1a187e4c07ae445d3b2
OlhaSi <liush.olga@gmail.com>
2021-04-06 00:12:19 +0200

    add recursion functions (open nested array, get factorial) and fibonacci function

--- recursion.js
+++ recursion.js
@@ -1,7 +1,7 @@
 /* ---- RECURSION ---- */
 
 let array = [3, 4, 6, 7];
-let array2 = [ 3, 4, 5, [6, [7], 4], 6, 75];
+let array2 = [3, 4, 5, [6, [7], 4], 6, 75];
 let i = 0;
 
 // TODO: print array
@@ -33,7 +33,64 @@ function recursion2(arr) {
 
 console.log(recursion2(array2));
 
+// second variant
+function printArray(arrayToOpen) {
+    if (!Array.isArray(arrayToOpen))
+        return;
 
+    for (let i = 0; i < arrayToOpen.length; i++) {
+        let value = arrayToOpen[i];
+
+        if (Array.isArray(value)) {
+            printArray(value);
+            continue;
+        }
+
+        if (typeof value === 'object' && value !== null) {
+            let arrayOfObj = Object.values(value);
+            printArray(arrayOfObj);
+            continue;
+        }
+
+        console.log(value);
+    }
+}
+
+console.log('**************');
+let arrayToOpen = [3, 4, 5, [6, [7], 4], 6, null, {name: 'Dan', car: true}, 75];
+
+printArray(arrayToOpen);
+console.log('**************');
+
+
+// TODO: get factorial of number
+console.time('my');
+
+function factorial(number) {
+    let res = 1;
+    for (let i = 1; i <= number; i++) {
+        res *= i;
+    }
+    return res;
+}
+
+console.log(factorial(3));
+console.timeEnd('my');
+
+// TODO: get factorial using recursion
+console.time('my1');
+
+function factorial2(n) {
+    if (n > 1) {
+        return n * (factorial2(n - 1)); // 5 * 4 * 3 * 2 * 1
+    }
+    return 1;
+
+    // return (n > 1) ? (n * (factorial2(n - 1))) : 1;
+}
+
+console.log(factorial2(0));
+console.timeEnd('my1');
 
 
 /* ----- CALLBACK ----- */
@@ -89,3 +146,20 @@ console.log(clean(true, (error, time, reward) => {
         return `Your reward is ${reward} \n${time}`;
     }
 }));
+
+
+/* ----- FIBONACCI----- */
+
+// TODO: get fibonacci
+function fibonacci(number) {
+    let res = [1, 1];
+    for (let i = 2; i < number; i++) {
+        res[i] = res[i - 1] + res[i - 2];
+        // res.push(res[i - 1] + res[i - 2]);
+    }
+
+    console.log(res);
+    return res[number - 1];
+}
+
+console.log(fibonacci(4));
